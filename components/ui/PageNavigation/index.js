8cce338390293b88fe0e89bf9341c31621beb8b6
Sergio Mattei <me@sergiomattei.com>
2020-01-18 04:02:49 -0400

    Created a PageNav component

---
+++ components/ui/PageNavigation/index.js
@@ -0,0 +1,77 @@
+import "./index.scss";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ActiveLink from "~/components/ActiveLink";
+import OutboundLink from "~/components/OutboundLink";
+import React from "react";
+
+class PageNavigation extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            expanded: false
+        };
+    }
+
+    onClickCloseExpand = () => {
+        if (this.state.expanded) {
+            this.setState({ expanded: !this.state.expanded });
+        }
+    };
+
+    render() {
+        return (
+            <nav
+                className="navbar navbar-secondary"
+                aria-label="main navigation"
+            >
+                <div className={"container"}>
+                    <div className={"flex v-center flex-gap-big"}>
+                        <div className={"flex v-center"}>
+                            <strong>{this.props.title}</strong>
+
+                            <div
+                                className="navbar-burger"
+                                onClick={e =>
+                                    this.setState({
+                                        expanded: !this.state.expanded
+                                    })
+                                }
+                            >
+                                <span />
+                                <span />
+                                <span />
+                            </div>
+                        </div>
+                        <div
+                            className={
+                                this.state.expanded
+                                    ? "navbar-menu is-active"
+                                    : "navbar-menu"
+                            }
+                        >
+                            <div
+                                className="navbar-start"
+                                onClick={this.onClickCloseExpand}
+                            >
+                                {this.props.children}
+                            </div>
+
+                            <div
+                                className="navbar-end"
+                                onClick={this.onClickCloseExpand}
+                            >
+                                {this.props.end ? this.props.end : null}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
+
+PageNavigation.propTypes = {};
+
+export default PageNavigation;
