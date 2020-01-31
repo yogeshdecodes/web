4a7d0485be6817643d50ee3f493f71a2ad895c68
Sergio Mattei <me@sergiomattei.com>
2020-01-31 12:22:08 -0400

    Optimize change

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import React from "react";
-import NextDocument from "next/document";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+
 import { ServerStyleSheet } from "styled-components";
 
 // Sadly we need this as long as we use styled-components. Fuck that.
@@ -29,5 +30,19 @@ export default class Document extends NextDocument {
         } finally {
             sheet.seal();
         }
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <script>{`(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;})(window,document.documentElement,'async-hide','dataLayer',4000,{'GTM-XXXXXX':true});`}</script>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
     }
 }
