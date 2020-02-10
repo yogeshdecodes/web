7eaff4757907e3e86812539c1dfb609ad9acb138
Sergio Mattei <me@sergiomattei.com>
2020-02-10 09:33:01 -0400

    Complete attendee panel

--- pages/event-attendee-panel/index.js
+++ pages/event-attendee-panel/index.js
@@ -22,8 +22,7 @@ class EventAttendeePanel extends Component {
                 href={`https://twitter.com/share?text=${encodeURIComponent(
                     text
                 )}&url=${url}`}
-                className="button is-small is-rounded"
-                target="_blank"
+                className="btn btn-small btn-light"
             >
                 <span className="icon">
                     <FontAwesomeIcon icon={["fab", "twitter"]} />
@@ -76,137 +75,135 @@ class EventAttendeePanel extends Component {
                             Makerlog lets you log tasks and track your progress
                             on your hackathon projects.
                         </p>
-                        <div className="flex v-center w-full">
-                            <OutboundLink
-                                className="btn btn-small btn-light"
-                                to={"/welcome"}
-                            >
-                                <span className="icon">
-                                    <FontAwesomeIcon icon={"check-circle"} />
-                                </span>{" "}
-                                Tutorial
-                            </OutboundLink>
+                        <div className="h-list flex center spaced v-center mt-5">
+                            <div>
+                                <OutboundLink
+                                    className="btn btn-small btn-light"
+                                    to={"/welcome"}
+                                >
+                                    <span className="icon">
+                                        <FontAwesomeIcon
+                                            icon={"check-circle"}
+                                        />
+                                    </span>{" "}
+                                    Tutorial
+                                </OutboundLink>
+                            </div>
 
-                            <OutboundLink
-                                className="button is-rounded is-small"
-                                to={"/log"}
-                            >
-                                <span className="icon">
-                                    <FontAwesomeIcon icon={"check-square"} />
-                                </span>{" "}
-                                Log
-                            </OutboundLink>
+                            <div>
+                                <OutboundLink
+                                    className="btn btn-small btn-light"
+                                    to={"/log"}
+                                >
+                                    <span className="icon">
+                                        <FontAwesomeIcon
+                                            icon={"check-square"}
+                                        />
+                                    </span>{" "}
+                                    Log
+                                </OutboundLink>
+                            </div>
 
-                            <OutboundLink
-                                className="button is-rounded is-small"
-                                to={"/apps"}
-                            >
-                                <span className="icon">
-                                    <FontAwesomeIcon icon={"ship"} />
-                                </span>{" "}
-                                Integrations & Apps
-                            </OutboundLink>
+                            <div>
+                                <OutboundLink
+                                    className="btn btn-small btn-light"
+                                    to={"/apps"}
+                                >
+                                    <span className="icon">
+                                        <FontAwesomeIcon icon={"ship"} />
+                                    </span>{" "}
+                                    Integrations & Apps
+                                </OutboundLink>
+                            </div>
 
-                            <OutboundLink
-                                className="button is-rounded is-small"
-                                to={"/discussions"}
-                            >
-                                <span className="icon">
-                                    <FontAwesomeIcon icon={"comments"} />
-                                </span>{" "}
-                                Forum
-                            </OutboundLink>
+                            <div>
+                                <OutboundLink
+                                    className="btn btn-small btn-light"
+                                    to={"/discussions"}
+                                >
+                                    <span className="icon">
+                                        <FontAwesomeIcon icon={"comments"} />
+                                    </span>{" "}
+                                    Forum
+                                </OutboundLink>
+                            </div>
                         </div>
                         <hr />
-                        <h1 className="title is-6">
+                        <h4>
                             <Emoji emoji="🚨" /> Team & product setup
-                        </h1>
-                        <h3 className="subtitle is-7">
+                        </h4>
+                        <p>
                             You can add your product to Makerlog to allow judges
                             to see it and track your progress more effectively.
                             You can also add your team to the product.
-                        </h3>
-                        <div className="buttons">
-                            <OutboundLink
-                                className="button is-primary is-rounded is-small"
-                                to={"/products"}
-                            >
-                                <span className="icon">
-                                    <FontAwesomeIcon icon={"plus"} />
-                                </span>{" "}
-                                Add a product
-                            </OutboundLink>
+                        </p>
+                        <div className="h-list flex center spaced v-center mt-5">
+                            <div>
+                                <OutboundLink
+                                    className="btn btn-small btn-light"
+                                    to={"/products"}
+                                >
+                                    <span className="icon">
+                                        <FontAwesomeIcon icon={"plus"} />
+                                    </span>{" "}
+                                    Add a product
+                                </OutboundLink>
+                            </div>
                         </div>
                         <hr />
-                        <h1 className="title is-6">Calendar setup</h1>
-                        <h3 className="subtitle is-7">
-                            Here's your timezone adjusted calendar files.
-                        </h3>
-                        <button
-                            className="button is-rounded is-small"
-                            onClick={this.getICalFiles}
-                        >
-                            <span className="icon">
-                                <FontAwesomeIcon icon="calendar-check" />
-                            </span>{" "}
-                            iCal event
-                        </button>
-                        &nbsp;
-                        <OutboundLink
-                            className="button is-rounded is-small"
-                            to={this.getGCalUrl()}
-                        >
-                            <span className="icon">
-                                <FontAwesomeIcon icon="calendar-check" />
-                            </span>{" "}
-                            Google Calendar event
-                        </OutboundLink>
+                        <h4>Calendar setup</h4>
+                        <p>Here's your timezone adjusted calendar files.</p>
+                        <div className="h-list flex center spaced v-center mt-5">
+                            <div>
+                                <button
+                                    className="btn btn-small btn-light"
+                                    onClick={this.getICalFiles}
+                                >
+                                    <span className="icon">
+                                        <FontAwesomeIcon icon="calendar-check" />
+                                    </span>{" "}
+                                    iCal event
+                                </button>
+                            </div>
+                            <div>
+                                <OutboundLink
+                                    className="btn btn-small btn-light"
+                                    to={this.getGCalUrl()}
+                                >
+                                    <span className="icon">
+                                        <FontAwesomeIcon icon="calendar-check" />
+                                    </span>{" "}
+                                    Google Calendar event
+                                </OutboundLink>
+                            </div>
+                        </div>
                         <hr />
-                        <h1 className="title is-6">Livestreaming setup</h1>
-                        <h3 className="subtitle is-7">
+                        <h4>Livestreaming setup</h4>
+                        <p>
                             Here's how to setup your livestreaming experience.
-                        </h3>
-                        <OutboundLink
-                            className="button is-rounded is-small"
-                            to={"/apps/shipstreams/"}
-                        >
-                            <span className="icon">
-                                <FontAwesomeIcon icon={"ship"} />
-                            </span>{" "}
-                            Set up livestreaming
-                        </OutboundLink>
+                        </p>
+                        <div className="h-list flex center spaced v-center mt-5">
+                            <div>
+                                <OutboundLink
+                                    className="btn btn-small btn-light"
+                                    to={"/apps/shipstreams/"}
+                                >
+                                    <span className="icon">
+                                        <FontAwesomeIcon icon={"ship"} />
+                                    </span>{" "}
+                                    Set up livestreaming
+                                </OutboundLink>
+                            </div>
+                        </div>
                         <hr />
-                        <h1 className="title is-6">Tweet material</h1>
-                        <h3 className="subtitle is-7">
+                        <h4>Tweet material</h4>
+                        <p>
                             Here's a little bit of tweet material to spread the
                             word of your attendance!
-                        </h3>
-                        {this.renderTweetButton()}
-                        <hr />
-                        <h1 className="title is-6">
-                            Details from the organizer
-                        </h1>
-                        <h3 className="subtitle is-7">
-                            Here's all you need to know about the event.
-                        </h3>
-                        <button
-                            className="button is-text is-small"
-                            onClick={e =>
-                                this.setState({
-                                    detailsOpen: !this.state.detailsOpen
-                                })
-                            }
-                        >
-                            <span className="icon">
-                                <FontAwesomeIcon icon="eye" />
-                            </span>{" "}
-                            Toggle details
-                        </button>
-                        {this.state.detailsOpen && (
-                            <div className="content">
-                                <Markdown body={item.details} />
-                            </div>
-                        )}
+                        </p>
+                        <div className="h-list flex center spaced v-center mt-5">
+                            <div>{this.renderTweetButton()}</div>
+                        </div>
                     </div>
                 </div>
             </EventsPageLayout>
