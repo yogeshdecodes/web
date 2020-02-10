7ce078489f7b61631f292aa7032e45853686885a
Sergio Mattei <me@sergiomattei.com>
2020-02-10 11:34:24 -0400

    Add "buy" link to sidebar ads

--- components/sidebar/components/AdCard.js
+++ components/sidebar/components/AdCard.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Ad from "~/components/Ad";
+import OutboundLink from "~/components/OutboundLink";
 
 export default () => {
     return (
         <div className="AdCard sidebar-item">
             <h3>Indie ad</h3>
             <h4 className="subtitle has-text-grey">
-                <a>Put your product here!</a>
+                <OutboundLink to="https://makerlog.io/intravert">
+                    Put your product here!
+                </OutboundLink>
             </h4>
             <div className="card">
                 <div className="card-content">
