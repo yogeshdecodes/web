973773a70269530e290cafd3ba546ecbde713669
Sergio Mattei <me@sergiomattei.com>
2020-07-15 19:28:01 -0400

    Update task state on stream

--- features/stream/components/Task/Task.js
+++ features/stream/components/Task/Task.js
@@ -74,6 +74,13 @@ class Task extends React.Component {
         };
     }
 
+    componentDidUpdate(prevProps) {
+        // Typical usage (don't forget to compare props):
+        if (this.props.task.updated_at !== prevProps.task.updated_at) {
+            this.setState({ task: this.props.task });
+        }
+    }
+
     onDelete = () => {
         this.setState({ deleted: true });
     };
