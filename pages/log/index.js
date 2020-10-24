156062770fbd9f68378b15b7ecbd37597ead6219
Sergio Mattei <me@sergiomattei.com>
2020-10-24 14:38:19 -0400

    Fix log for now.

--- pages/log/index.js
+++ pages/log/index.js
@@ -9,7 +9,6 @@ import FeedSwitcher from "../../features/feeds/FeedSwitcher";
 import MainFeed from "../../features/feeds/MainFeed";
 import { CardEditor } from "../../features/editor";
 import { prefetchActivity } from "../../features/feeds/KeyActivityFeed";
-import RemindersSetup from "../../features/reminders/RemindersSetup";
 import { GlobalStream } from "../../features/stream";
 
 class StreamPage extends React.Component {
@@ -19,6 +18,7 @@ class StreamPage extends React.Component {
                 footer: false
             },
             ...(await prefetchData()),
+            activitiesPrefetch: await prefetchActivity("timeline", "-1"),
             discussionPrefetch: await prefetchThreads()
         };
     }
@@ -45,7 +45,6 @@ class StreamPage extends React.Component {
                             ) : (
                                 <CardEditor />
                             )}
-                            <RemindersSetup />
                             <div className="alert is-warning">
                                 <div className="alert-body">
                                     <strong>Important note</strong>
